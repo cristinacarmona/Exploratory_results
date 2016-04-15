0c516bae164872c8d44dbd86261abff4ae742ddf
cristinacarmona <mcristina.carmona@gmail.com>
2016-04-15 11:42:26 +0100

    Debugged created file with merge populations, v3

--- R/Merge population files.R
+++ R/Merge population files.R
@@ -14,7 +14,7 @@
   
 
   working.list <- df.list
-  names(working.list) <- c("all","ceuta","ceuta.maio","mad","maio","tuzla")
+  names(working.list) <- c("all1","all2","ceuta","ceuta.maio","mad","maio","tuzla1","tuzla")
   
   attach(working.list)
   
@@ -41,18 +41,25 @@ mad$hatching_date.r<-ifelse(is.na(mad$hatching_date.r) & !is.na(mad$hatch_date),
 colnames(mad)[38]<-"hatch"
 mad$hatching_date.r<-as.Date(mad$hatching_date.r, "%d/%m/%Y")
 
+
 ind<-which(is.na(mad$layingdate))
-mad$estimated.ld<-as.Date(mad$estimated.ld)
+mad$estimated.ld<-as.Date(mad$estimated.ld,"%d/%m/%Y")
 mad$estimated.ld[ind]<-mad$hatching_date.r[ind]-25
-mad$layingdate<-as.Date(mad$layingdate)
+mad$layingdate<-as.Date(mad$layingdate,"%d/%m/%Y")
 mad$layingdate[ind]<-mad$estimated.ld[ind]
 
+# cols <- c(85,84,82,81,80,79,77)
+# both2[,cols]<-lapply(both2[,cols], as.Date, origin="1970-01-01")
+library(lubridate)
+dates<-sapply(mad, is.Date)
+mad[,dates]<-lapply(mad[,dates], as.character)
+str(mad)
 
 tuzla$population <- "Tuzla"
 str(tuzla)
 tuzla$nest<-as.character(tuzla$nest)
 tuzla$nest.id <- paste(tuzla$population, tuzla$year, tuzla$species, tuzla$site, tuzla$nest, sep="-")
-
+colnames(tuzla)[11]<-"hatch"
 #-----------------------------------------Merge
 #merge datasets
 library(gtools)
@@ -60,7 +67,7 @@ library(gtools)
 all.pops<-smartbind(ceuta, maio, mad, tuzla)
 str(all.pops)
 
-write.csv(all.pops, "F:/Plovers/3rd Chapter/Exploratory_results/output/allpops_bschedule_14April2016.csv")
+write.csv(all.pops, "F:/Plovers/3rd Chapter/Exploratory_results/output/allpops_bschedule_15April2016_V3.csv")
 
 
 
